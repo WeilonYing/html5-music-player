52c8d7ea2b336b86e2b24d9083c887e433f9e4a3
WeilonYing <weilony@hotmail.com>
2017-07-25 13:25:44 +1000

    Flask now passes JSON of song names to webpage

--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
-from flask import render_template, redirect, request
+from flask import jsonify, Markup, render_template, redirect, request
 from random import randint
+import json
 from app import app
 
 @app.route('/')
 @app.route('/index/')
 def index():
     return render_template('index.html',
-                            pagename = 'Home')
+        pagename = 'Home',
+		songlist = ['Exile Vilify.mp3', '09 - Shake That - Band-Maid.mp3'])
+		
+def getSongList():
+	output = ["Exile Vilify.mp3", "09 - Shake That - Band-Maid.mp3"]
+	print (output)
+	return "[" + ",".join(output) + "]"
