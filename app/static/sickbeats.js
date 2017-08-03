bfa85a456f4babc03bbdf40d53f594550bf99ca1
WeilonYing <weilony@hotmail.com>
2017-08-03 10:54:20 +1000

    Added support for file directories (preparation for artist and album support)

--- app/static/sickbeats.js
+++ app/static/sickbeats.js
@@ -3,51 +3,77 @@ console.log(musiclocation);
 
 var player = document.getElementById("play");
 var index = 0;
+var songQueue = [];
+var timerStarted = false;
 
-if (self.songs.length > 0) {
-	player.preload = "auto";
-	player.playbackRate = 1.0;
-	playSong(self.songs[self.index]);
-	
-	var timer = setInterval(switchSong, 1000);
+//Initialise song queue
+if (self.files != null) {
+    initSongQueue();
+    player.preload = "auto";
+    player.playbackRate = 1.0;
+}
+
+function initSongQueue() {
+    self.addAllSongs(musiclocation, self.files.children);
+    if (self.songQueue.length > 0) {
+        self.setSong(self.songQueue[index++]);
+    }
+}
+
+function addAllSongs(parentdir, directory) {
+    for (var key in directory) {
+        console.log(key);
+        let object = directory[key];
+        if (object.type == "file") {
+            this.songQueue.push(parentdir + "/" + object.name);
+        }
+        if (object.type == "directory") {
+            this.addAllSongs(parentdir + "/" + object.name, object.children);
+        }
+    }
 }
 
 function switchSong() {
-	if (self.player.ended && self.index < self.songs.length - 1) {
-		playSong(self.songs[++index]);
-	}
+    if (self.player.ended && self.index < self.songQueue.length - 1) {
+        setSong(self.songQueue[++index]);
+        self.player.play();
+    }
 }
 
-function playSong(songName) {
-	self.player.src = self.musiclocation + songName;
-	player.play();
-	document.getElementById("now-playing").innerText = songName;
+function setSong(songName) {
+    self.player.src = songName;
+    document.getElementById("now-playing").innerText = songName;
+
+    if (!this.timerStarted) {
+        var timer = setInterval(switchSong, 1000);
+    }
 }
 
 function nextSong() {
-	if (self.index < self.songs.length - 1) {
-		playSong(self.songs[++index]);
-	}
+    if (self.index < self.songQueue.length - 1) {
+        setSong(self.songQueue[++index]);
+        self.player.play();
+    }
 }
 
 function prevSong() {
-	if (self.index > 0) {
-		playSong(self.songs[--index]);
-	}
+    if (self.index > 0) {
+        setSong(self.songQueue[--index]);
+        self.player.play();
+    }
 }
 
 function shuffleSongs() {
-	if (self.songs.length > 0) {
-		self.index = 0
-		for (let i = 0; i < self.songs.length; i++) {
-			let newInd = Math.floor(Math.random() * i);
-			// swap
-			let temp = self.songs[i];
-			self.songs[i] = self.songs[newInd];
-			self.songs[newInd] = temp;
-		}
-		playSong(self.songs[self.index]);
-	}
-	
-}
+    if (self.songQueue.length > 0) {
+        self.index = 0
+        for (let i = 0; i < self.songQueue.length; i++) {
+            let newInd = Math.floor(Math.random() * i);
+            // swap
+            let temp = self.songQueue[i];
+            self.songQueue[i] = self.songQueue[newInd];
+            self.songQueue[newInd] = temp;
+        }
+        setSong(self.songQueue[self.index]);
+    }
 
+}
--- app/views.py
+++ app/views.py
@@ -3,21 +3,28 @@ from app import app
 import os
 import re
 
+initialised = False
+musicFiles = None
+
+def getMusicFiles(path): # credit to https://stackoverflow.com/a/25226267
+
+    d = {'name': os.path.basename(path)}
+    if os.path.isdir(path):
+        d['type'] = "directory"
+        d['children'] = [getMusicFiles(os.path.join(path,x)) for x in os.listdir(path)]
+    else:
+        d['type'] = "file"
+    return d
+
 @app.route('/')
 @app.route('/index/')
 def index():
     return render_template('index.html',
         pagename = 'Home',
-		songlist = getSongList())
-		
-def getSongList():
-	output = []
-	
-	static_path = os.path.join(app.static_folder, "music/")
-	files = os.listdir(static_path)
-	mpeg_regex = re.compile(r'^.*\.mp3$')
-	for name in files:
-		if mpeg_regex.match(name) is not None:
-			output.append(name)
-	output.sort()
-	return output
+		files = musicFiles)
+
+# initialise file system
+if not initialised:
+    path = os.path.join(app.static_folder, "music/")
+    musicFiles = getMusicFiles(path)
+    initialised = True
